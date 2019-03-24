5f74d65e58aa7b23b19ab4cf03a394bebbe65f6f
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-03-24 20:06:09 -0300

    JPa setup

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,6 +1,9 @@
 package com.in28minutes.microservices.currencyexchangeservice.controller;
 
 import com.in28minutes.microservices.currencyexchangeservice.model.ExchangeValue;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +13,11 @@ import java.math.BigDecimal;
 @RestController
 public class CurrencyExchangeController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
-        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+        return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65), Integer.parseInt(environment.getProperty("local.server.port")));
     }
 }
--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/model/ExchangeValue.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/model/ExchangeValue.java
@@ -5,17 +5,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ExchangeValue {
 
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
+    @Column(name = "conversion_multiple")
     private BigDecimal conversionMultiple;
+
+    private int port;
 
 }
