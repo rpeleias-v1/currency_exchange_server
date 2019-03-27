d900aef6e5ab0574e3529091fb9723dbd133e161
Rodrigo Peleias <rodrigo.peleias@clickbus.com>
2019-03-27 14:07:25 -0300

    Added spring eureka and zuul

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -2,12 +2,14 @@ package com.in28minutes.microservices.currencyexchangeservice.controller;
 
 import com.in28minutes.microservices.currencyexchangeservice.model.ExchangeValue;
 import com.in28minutes.microservices.currencyexchangeservice.repository.ExchangeValueRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class CurrencyExchangeController {
 
@@ -21,6 +23,8 @@ public class CurrencyExchangeController {
     public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
         ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
         exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+
+        log.info("{}", exchangeValue);
 
         return exchangeValue;
     }
